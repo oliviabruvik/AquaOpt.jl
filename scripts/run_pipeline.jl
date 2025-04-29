7fd2fdcae25fde9b1dfe066e4d2baabefe85bece
Olivia Beyer Bruvik <74213092+oliviabruvik@users.noreply.github.com>
2025-04-29 14:48:33 -0700

    Cleaned up main function

--- scripts/run_pipeline.jl
+++ scripts/run_pipeline.jl
@@ -1,65 +1,65 @@
 include("../src/cleaning.jl")
 include("../src/optimize_mdp.jl")
-include("../src/optimize_sarsop.jl")
 include("../src/plot_views.jl")
-include("../src/utils.jl")
+include("../src/simulations.jl")
 
 # Import required packages
 using DiscreteValueIteration
+using Logging
 using POMDPs
 using Plots
 using NativeSARSOP: SARSOPSolver
 
+# Environment variables
 ENV["PLOTS_BROWSER"] = "true"
+ENV["PLOTS_BACKEND"] = "plotlyjs"
 
-# Load and clean data
-df = load_and_clean("data/raw/licedata.csv")
-
-# Plot sealice levels over time
-sealice_levels_over_time_plot = plot_sealice_levels_over_time(df)
-savefig(sealice_levels_over_time_plot, "results/figures/sealice_levels_over_time.png")
-
-# Evaluate policies
-lambda_values = 0.0:0.2:1.0 # 0.0:0.05:1.0
-num_episodes = 10 # 1000
-steps_per_episode = 50 # 500
-
-# Heuristic Policy
-test_optimizer(
-    lambda_values,
-    ValueIterationSolver(max_iterations=30),
-    episodes=num_episodes,
-    steps_per_episode=steps_per_episode,
-    convert_to_mdp=true,
-    plot_name="Heuristic Policy"
+const CONFIG = Dict(
+    :lambda_values => 0.0:0.2:1.0, # 0.0:0.05:1.0
+    :num_episodes => 100, # 1000,
+    :steps_per_episode => 50 # 100
 )
 
-# MDP
-test_optimizer(
-    lambda_values,
-    ValueIterationSolver(max_iterations=30),
-    episodes=num_episodes,
-    steps_per_episode=steps_per_episode,
-    convert_to_mdp=true,
-    plot_name="MDP Policy"
-)
+run_algorithms = true
 
-# SARSOP
-test_optimizer(
-    lambda_values,
-    SARSOPSolver(; max_time=10.0),
-    episodes=num_episodes,
-    steps_per_episode=steps_per_episode,
-    convert_to_mdp=false,
-    plot_name="SARSOP Policy"
-)
+# Create results directories
+mkpath("results/figures")
+mkpath("results/data")
 
-# QMDP
-test_optimizer(
-    lambda_values,
-    QMDPSolver(max_iterations=30),
-    episodes=num_episodes,
-    steps_per_episode=steps_per_episode,
-    convert_to_mdp=false,
-    plot_name="QMDP Policy"
-)
+function main(run_algorithms=false)
+
+    @info "Loading and cleaning data"
+    df = load_and_clean("data/raw/licedata.csv")
+    sealice_levels_over_time_plot = plot_sealice_levels_over_time(df)
+
+    # Run algorithms
+    if run_algorithms
+        policies = [
+            ("Heuristic Policy", ValueIterationSolver(max_iterations=30), true),
+            ("VI Policy", ValueIterationSolver(max_iterations=30), true),
+            ("SARSOP Policy", SARSOPSolver(max_time=10.0), false),
+            ("QMDP Policy", QMDPSolver(max_iterations=30), false)
+        ]
+
+        for (policy_name, solver, convert_to_mdp) in policies
+            @info "Running $policy_name"
+            test_optimizer(
+                CONFIG[:lambda_values],
+                solver,
+                episodes=CONFIG[:num_episodes],
+                steps_per_episode=CONFIG[:steps_per_episode],
+                convert_to_mdp=convert_to_mdp,
+                plot_name=policy_name
+            )
+        end
+    end
+
+    # Plot overlay of all policies
+    @info "Generating result plots"
+    overlay_plot = plot_mdp_results_overlay(CONFIG[:num_episodes], CONFIG[:steps_per_episode])
+    policy_sealice_comparison_plot = plot_policy_sealice_levels(CONFIG[:num_episodes], CONFIG[:steps_per_episode])
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main("--run" in ARGS)
+end
